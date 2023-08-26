92a66078ca73bf5197819354ce5659cda40455ac
nstirnemann7 <nicolas@brainhi.com>
2023-08-26 19:17:43 -0300

    First Commit

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("097e3658aea0debbbe554d0ac3aec42f23b8cdc8d973e344242ba811e69609b9" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ early-init.el
@@ -0,0 +1,73 @@
+(setq package-enable-at-startup nil)
+
+;; UNICODE
+(prefer-coding-system       'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-language-environment   'utf-8)
+
+;; INTERFACE
+(global-auto-revert-mode)
+
+(menu-bar-mode           0)
+(electric-pair-mode      1)
+(delete-selection-mode   1)
+(temp-buffer-resize-mode t)
+(xterm-mouse-mode        1)
+(scroll-bar-mode         0)
+(fringe-mode             0)
+(tool-bar-mode           0)
+
+;; ALIASES & VARIABLES
+(defalias 'yes-or-no-p 'y-or-n-p)
+(defvar custom-file-path (expand-file-name "custom.el" user-emacs-directory))
+
+
+;; CUSTOMISATIONS
+(setq-default truncate-lines  -1
+              indent-tabs-mode nil
+              tab-width        2)
+
+(setq ring-bell-function              'ignore
+      inhibit-splash-screen           t
+      initial-scratch-message         nil
+      select-enable-primary           t
+      select-enable-clipboard         t
+      max-mini-window-height          3
+      warning-minimum-level           :error
+
+      ;; MODE LINE
+      line-number-mode                t
+      column-number-mode              t
+
+      ;; DISABLE TAB BAR
+      tab-bar-close-button-show       nil
+      tab-bar-mode                    nil
+      tab-bar-show                    nil
+
+      ;; SCROLLING
+      scroll-margin                   0
+      scroll-conservatively           100000
+      scroll-preserve-screen-position 1
+      auto-window-vscroll             nil
+
+      ;; EDIT MODE
+      require-final-newline           t
+
+      ;; PERFORMANCE
+      gc-cons-threshold               100000000
+      read-process-output-max         (* 1024 1024)  ;; 1mb
+
+      ;; BACKUP
+      backup-directory-alist         `((".*" . ,temporary-file-directory))
+      auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
+      default-directory              "~/"
+      create-lockfiles               nil
+      custom-file                    custom-file-path
+
+      ;; PACKAGE ARCHIVES
+      package-archives               '(("gnu-elpa" . "https://elpa.gnu.org/packages/")
+                                       ("jcs-elpa" . "https://jcs-emacs.github.io/jcs-elpa/packages/")
+                                       ("melpa"    . "https://melpa.org/packages/"))
+      package-archive-priorities     '(("gnu-elpa" . 0)
+                                       ("jcs-elpa" . 5)
+                                       ("melpa"    . 10)))
